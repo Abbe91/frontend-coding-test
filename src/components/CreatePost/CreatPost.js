63d935735ad62bbc802d14b773ad1e8b6b5d2c37
abbe91 <abdullah.abo.alhaijja@hotmail.com>
2023-05-16 09:59:34 +0200

    Send likes and text

--- src/components/CreatePost/CreatPost.js
+++ src/components/CreatePost/CreatPost.js
@@ -1,18 +1,95 @@
+// import { ref } from "vue";
+// import { appId } from "../ApiConnection/api";
+
+// export default {
+//   name: "CreatePost",
+//   setup() {
+//     const userId = "646330845f3ddc08319a9301";
+//     const newPost = ref({
+//       text: "",
+//       likes: 0,
+//       tags: [],
+//       owner: userId,
+//     });
+//     const tagInput = ref("");
+//     const showNotification = ref(false);
+//     const showNotificationFalse = ref(false);
+
+//     const createPost = async () => {
+//       try {
+//         const response = await fetch(
+//           "https://dummyapi.io/data/v1/post/create",
+//           {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//               "app-id": appId,
+//             },
+//             body: JSON.stringify(newPost.value),
+//           }
+//         );
+
+//         if (!response.ok) {
+//           throw new Error("Failed to create post");
+//         }
+
+//         const createdPost = await response.json();
+//         console.log("Created Post ID:", createdPost.id);
+
+//         // Reset the newPost object
+//         newPost.value.text = "";
+//         newPost.value.likes = 0;
+//         newPost.value.tags = [];
+//         showNotification.value = true;
+//       } catch (error) {
+//         console.error(error);
+//         showNotificationFalse.value = true;
+//       }
+//     };
+
+//     const addTag = () => {
+//       const tag = tagInput.value.trim();
+//       if (tag) {
+//         newPost.value.tags.push(tag);
+//         tagInput.value = "";
+//       }
+//     };
+
+//     const removeTag = (index) => {
+//       newPost.value.tags.splice(index, 1);
+//     };
+
+//     return {
+//       newPost,
+//       tagInput,
+//       showNotification,
+//       showNotificationFalse,
+//       createPost,
+//       addTag,
+//       removeTag,
+//     };
+//   },
+// };
+
+//new test
+
 import { ref } from "vue";
 import { appId } from "../ApiConnection/api";
+
 export default {
   name: "CreatePost",
   setup() {
     const userId = "646330845f3ddc08319a9301";
     const newPost = ref({
-      title: "",
-      image: "",
       text: "",
       likes: 0,
       tags: [],
       owner: userId,
     });
     const tagInput = ref("");
+    const showNotification = ref(false);
+    const showNotificationFalse = ref(false);
+
     const createPost = async () => {
       try {
         const response = await fetch(
@@ -24,12 +101,8 @@ export default {
               "app-id": appId,
             },
             body: JSON.stringify({
-              owner: userId,
-              post: {
-                title: newPost.value.title,
-                image: newPost.value.image,
-                text: newPost.value.text,
-              },
+              ...newPost.value,
+              tags: newPost.value.tags,
             }),
           }
         );
@@ -39,18 +112,16 @@ export default {
         }
 
         const createdPost = await response.json();
-        console.log("Created Post: 01", createdPost);
-
-        // Push the created post into the posts array
-        console.log("Created Post: 02", newPost.value);
+        console.log("Created Post ID:", createdPost.id);
 
         // Reset the newPost object
-        newPost.value.title = "";
-        newPost.value.image = "";
         newPost.value.text = "";
+        newPost.value.likes = 0;
         newPost.value.tags = [];
+        showNotification.value = true;
       } catch (error) {
-        console.error("Failed to create post:", error);
+        console.error(error);
+        showNotificationFalse.value = true;
       }
     };
 
@@ -66,15 +137,12 @@ export default {
       newPost.value.tags.splice(index, 1);
     };
 
-    const onImageChange = (event) => {
-      const file = event.target.files[0];
-      newPost.value.image = URL.createObjectURL(file);
-    };
-
     return {
       newPost,
+      tagInput,
+      showNotification,
+      showNotificationFalse,
       createPost,
-      onImageChange,
       addTag,
       removeTag,
     };
--- src/components/CreatePost/CreatePost.vue
+++ src/components/CreatePost/CreatePost.vue
@@ -1,26 +1,42 @@
 <template>
   <div class="create-post">
     <h2>Create Post</h2>
-
-    <label for="title">Title</label>
-    <input type="text" id="title" v-model="newPost.title" required />
-
-    <label for="text">Text</label>
-    <textarea id="text" v-model="newPost.text" required></textarea>
-
-    <label for="image">Image URL</label>
-    <input type="text" id="image" v-model="newPost.image" required />
-
-    <label for="tags">Tags (separated by commas)</label>
-    <input type="text" id="tags" v-model="tagInput" @keydown.enter="addTag" />
-    <div class="tags-container">
-      <div v-for="(tag, index) in newPost.tags" :key="index" class="tag">
-        {{ tag }}
-        <span class="tag-remove" @click="removeTag(index)">x</span>
+    <form @submit.prevent="createPost">
+      <div>
+        <label for="text">Text</label>
+        <textarea id="text" v-model="newPost.text" required></textarea>
       </div>
+      <div>
+        <label for="likes">Likes</label>
+        <input type="number" id="likes" v-model="newPost.likes" required />
+      </div>
+      <div>
+        <label for="tags">Tags</label>
+        <input
+          type="text"
+          id="tags"
+          v-model="tagInput"
+          @keyup.enter="addTag"
+          placeholder="Enter a tag and press Enter"
+        />
+        <ul class="tag-list">
+          <li v-for="(tag, index) in newPost.tags" :key="index">
+            {{ tag }}
+            <button class="tag-remove-btn" @click="removeTag(index)">X</button>
+          </li>
+        </ul>
+      </div>
+      <button type="submit">Create Post</button>
+    </form>
+
+    <div v-if="showNotification">
+      <p>Post created successfully!</p>
+      <p>Created Post ID: {{ createdPostId }}</p>
     </div>
 
-    <button @click="createPost">Create Post</button>
+    <div v-if="showNotificationFalse">
+      <p>Failed to create post.</p>
+    </div>
   </div>
 </template>
 
